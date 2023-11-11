92e913f7e888afad94e7a6b1e0be5941a72d17cd
Imran940 <immoshaikh91@gmail.com>
2023-11-11 17:45:34 +0530

    added a check for the url

--- src/Conponents/Home/Home.jsx
+++ src/Conponents/Home/Home.jsx
@@ -15,8 +15,9 @@ export default function Home() {
     if (!isLogin) navigate("/login");
   }, []);
 
-  const handlePushNotification = async () => {
+  const handlePushNotification = async (e) => {
     try {
+      e.preventDefault();
       setLoading(true);
       const notify_link = document.getElementById("link").value;
       await pushNotification({ email, password, notify_link });
@@ -33,22 +34,27 @@ export default function Home() {
   return (
     <div className="m-10">
       {role == "admin" ? (
-        <div className="mt-[15em] flex flex-row justify-center	">
-          <input
-            type="text"
-            id="link"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-[50%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
-            placeholder="https://www.link.com/"
-            required
-          />
-          <button
-            className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-r-lg
-                            hover:opacity-75"
-            onClick={handlePushNotification}
-            disabled={loading}
+        <div className="mt-[15em] ">
+          <form
+            onSubmit={handlePushNotification}
+            className="flex flex-row justify-center	"
           >
-            {loading ? "Pushing..." : "Push"}
-          </button>
+            <input
+              type="url"
+              id="link"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-[50%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
+              placeholder="https://www.link.com/"
+              required
+              title="Please enter a proper url"
+            />
+            <button
+              className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-r-lg
+                            hover:opacity-75"
+              disabled={loading}
+            >
+              {loading ? "Pushing..." : "Push"}
+            </button>
+          </form>
         </div>
       ) : (
         <About />
